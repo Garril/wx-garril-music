c2fa08d76845b474eb23db1e8209dd7f14516f89
Kuoc <zev20010120@163.com>
2022-01-21 00:28:51 +0800

    Finish(HomeMusic)SongsList_SimpleShow

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 // app.js
 App({
-
+  globalData: {
+    screenWidth: 0,
+    screenHeight: 0
+  },
+  onLaunch() {
+    // 拿到机型信息
+    const info = wx.getSystemInfoSync()
+    this.globalData.screenHeight = info.screenHeight
+    this.globalData.screenWidth = info.screenWidth
+  }
 })
--- service/api_music.js
+++ service/api_music.js
@@ -5,9 +5,17 @@ export function getBanners() {
     type: 2
   })
 }
-// 获取榜单数据
+// 获取推荐歌单
 export function getRanking(idx) {
   return enhanceReuqest.get("/top/list",{
     idx
+  })
+}
+// 获取热门歌单
+export function getSongMenu(cat="全部", limit=8, offset=0) {
+  return enhanceReuqest.get("/top/playlist", {
+    cat, // 华语，古风，欧美，流行，全部
+    limit, // 默认50
+    offset // 用于分页，(评论页数-1)*50
   })
 }
