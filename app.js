aeae09205a2f0499ef2bb3469a57e99d0b626742
Kuoc <zev20010120@163.com>
2022-01-30 01:05:25 +0800

    Init_Login(Token__AND__Session_Key)

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 // app.js
+import { getLoginCode, codeToToken, checkToken, checkSession } from './service/api_login'
+import { TOKEN_KEY } from './constants/token-const'
+
 App({
   globalData: {
     screenWidth: 0,
@@ -7,7 +10,7 @@ App({
     navBarHeight: 44, // 这里设置了默认，非获取
     deviceRadio: 0 // 一般为1.77几的旧版屏幕 和 2.多的刘海屏
   },
-  onLaunch() {
+  async onLaunch() {
     // 拿到机型信息
     const info = wx.getSystemInfoSync()
     this.globalData.screenHeight = info.screenHeight
@@ -15,5 +18,22 @@ App({
     this.globalData.statusBarHeight = info.statusBarHeight
     const deviceRadio = info.screenHeight / info.screenWidth // 高/宽
     this.globalData.deviceRadio = deviceRadio
+    // 默认登录
+    const token = wx.getStorageSync(TOKEN_KEY)
+    // 有无token，且要判断是否过期，还有session_key是否过期
+    const checkResult = await checkToken(token)
+    // 微信自动判断session_key是否过期，不用传参数。看wx.checkSession的回调
+    const isSessionExpire = await checkSession()
+    if(!token || checkResult.errorCode || !isSessionExpire) {
+      this.loginAction()
+    }
+  },
+  async loginAction() {
+    // 获取code
+    const code = await getLoginCode()
+    // 将code发给服务器
+    const result = await codeToToken(code)
+    const token = result.token
+    wx.setStorageSync(TOKEN_KEY,token)
   }
 })
--- service/index.js
+++ service/index.js
@@ -1,11 +1,17 @@
 const BASE_URL = "http://123.207.32.32:9001"
 
+const LOGIN_BASE_URL = "http://123.207.32.32:3000"
+
 class EnhanceRequest {
-  request(url, method, params) {
+  constructor(baseURL) {
+    this.baseURL = baseURL
+  }
+  request(url, method, params, header = {}) {
     return new Promise((resolve, reject) => {
       wx.request({
-        url: BASE_URL + url,
+        url: this.baseURL + url,
         method: method,
+        header: header,
         data: params,
         success: function(res) { // 可直接 success(res) {...}
           resolve(res.data)
@@ -14,12 +20,18 @@ class EnhanceRequest {
       })
     })
   }
-  get(url, params) {
-    return this.request(url,"GET",params)
+  get(url, params, header) {
+    return this.request(url,"GET",params,header)
   }
-  post(url,data) {
-    return this.request(url,"POST",data)
+  post(url, data, header) {
+    return this.request(url,"POST",data,header)
   }
 }
-const enhanceRequest = new EnhanceRequest()
+const enhanceRequest = new EnhanceRequest(BASE_URL)
+
+const loginRequest = new EnhanceRequest(LOGIN_BASE_URL)
+
 export default enhanceRequest
+export {
+  loginRequest
+}
